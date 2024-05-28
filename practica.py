c7b9966152669e123da0b031a19188b0daf4ebff
Yordy Nuñez <yordierik05@gmail.com>
2024-05-28 09:27:28 -0500

    Se sube estructuras de control

--- practica.py
+++ practica.py
@@ -74,4 +74,36 @@ b = 2
 resultado = a <= b
 print(resultado)
 
+print("-----------------ESTRUCTURAS DE CONTROL---------------------")
 
+a = 10
+if a > 5:
+    print("Es mayor que 5")
+else:
+    print("No es mayor que 5")
+
+print("-----------------ESTRUCTURAS DE CONTROL IF-ELSE-IF-ELSE---------------------")
+
+a = 10
+if a > 0:
+    print("Es mayor que 5")
+elif a < 0:
+    print("No es mayor que 5")
+else:
+    print("No es mayor que 5")
+
+print("-----------------ESTRUCTURAS DE CONTROL WHILE---------------------")
+
+contador = 1
+while contador <= 5:
+    print(contador)
+    contador += 1	
+
+print("-----------------ESTRUCTURAS DE CONTROL FOR---------------------")
+frutas =["manzana", "banano", "naranja"]
+     
+for fruta in frutas:
+        print(fruta)
+
+for i in range(1, 6):
+     print(i)
