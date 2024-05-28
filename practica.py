a51eb66c4744c22797d7ef66a4ad8c1b16d4f64d
Yordy Nuñez <yordierik05@gmail.com>
2024-05-28 09:01:27 -0500

    Practica de python

---
+++ practica.py
@@ -0,0 +1,18 @@
+print("------------Suma----------------")
+a = 5
+b = 2
+resultado = a + b
+print(resultado)
+
+print("------------Resta----------------")
+a = 5
+b = 2
+resultado = a - b
+print(resultado)
+
+print("------------Multiplicación----------------")
+a = 5
+b = 2
+resultado = a * b
+print(resultado)
+
