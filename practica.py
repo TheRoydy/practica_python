4736fe76654747a136ac96c8d23ba0b536960096
Yordy Nuñez <yordierik05@gmail.com>
2024-05-28 09:15:04 -0500

    Se sube practica de operadores de python

--- practica.py
+++ practica.py
@@ -16,3 +16,62 @@ b = 2
 resultado = a * b
 print(resultado)
 
+print("------------División----------------")
+a = 5
+b = 2
+resultado = a / b
+print(resultado)
+
+print("------------División Entera----------------")
+a = 5
+b = 2
+resultado = a // b
+print(resultado)
+
+print("------------Modulo----------------")
+a = 5
+b = 2
+resultado = a % b
+print(resultado)
+
+print("------------Contador----------------")
+contador = 0
+contador += 1 
+print(contador)
+
+print("------------Igualdad----------------")
+a = 5
+b = 2
+resultado = a == b
+print(resultado)
+
+print("------------Desigualdad----------------")
+a = 5
+b = 2
+resultado = a != b
+print(resultado)
+
+print("------------Mayor que----------------")
+a = 5
+b = 2
+resultado = a > b
+print(resultado)
+
+print("------------Menor que----------------")
+a = 5
+b = 2
+resultado = a < b
+print(resultado)
+
+print("------------Mayor o igual que y menor o igual que----------------")
+a = 5
+b = 2
+resultado = a >= b
+print(resultado)
+
+a = 5
+b = 2
+resultado = a <= b
+print(resultado)
+
+
